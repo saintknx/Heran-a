20d1f5aa549959ff4aae568ba116fdc680c9a67c
Kaua Sousa <kauasantos2014s10@gmail.com>
2024-04-29 19:38:32 -0300

    Herança classes

---
+++ AlunoDeGraduacao.java
@@ -0,0 +1,39 @@
+public class AlunoDeGraduacao {
+    private String nome;
+    private int idade;
+    private double nota1, nota2, notaFinal;
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public double getNota1() {
+        return nota1;
+    }
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+    public double getNota2() {
+        return nota2;
+    }
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+    public void setNotaFinal(double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
+    
+
+}
---
+++ AlunoDePosGraduacao.java
@@ -0,0 +1,26 @@
+public class AlunoDePosGraduacao {
+    private String nome;
+    private int idade;
+    private char conceito;
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public char getConceito() {
+        return conceito;
+    }
+    public void setConceito(char conceito) {
+        this.conceito = conceito;
+    }
+    
+}
